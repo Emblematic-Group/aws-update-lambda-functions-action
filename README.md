11e92c4be3025cce97bc82264f66f56ca5252581
Marco Cavalli <cmarcos46@gmail.com>
2021-02-02 17:25:54 +0100

    Initialize Repo

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # aws-update-lambda-functions-action
+
+This action must be used when you want to update the lambda functions. It will fails if the stacks (s3, rds and lambdas) don’t exist.
+
+## Example workflows file
+```yaml
+name: "update_lambda_stack"
+on:
+  workflow_dispatch:
+    branches:
+    - awsstaging
+jobs:
+  deploy:
+    runs-on: ubuntu-latest
+    steps:
+    - uses: actions/checkout@v1
+    - name: Extract branch name
+      shell: bash
+      run: echo "::set-output name=branch::$(echo ${GITHUB_REF#refs/heads/})"
+      id: extract_branch
+    - uses: mirkesx/aws-update-lambda-stack-reach@main
+      env:
+        AWS_FOLDER: 'aws_cf_stack'
+        AWS_STACK_PREFIX: ${{ steps.extract_branch.output.branch }}
+        AWS_REGION_VALUE: 'eu-north-1'
+        AWS_ACCESS_KEY_ID_VALUE: ${{ secrets.AWS_ACCESS_KEY_ID_S3USER }}
+        AWS_SECRET_ACCESS_KEY_VALUE: ${{ secrets.AWS_SECRET_ACCESS_KEY_S3USER }}
+```
+
+## Example break-down
+This actions can be triggered only from the Action tab since it is a workflow_dispatch.
+
+The only branch allowed to dispatch this action is awsstaging (there will be more in the future).
+
+### Steps:
+- actions/checkout@v1 is used to allow the action to pull the repo
+-“Extract branch name” is used to retrieve the name of the branch
+- mirkesx/aws-update-lambda-stack-reach@main runs AWS CLI/SAM commands to update the lambda functions.
+
+### Env variables
+- AWS_FOLDER, which is the path to the folder in the repo where you can find the SAM templates
+- AWS_STACK_PREFIX, will be the prefix for all the three stacks. By default is the branch name and is retrieved in the second step
+- AWS_REGION_VALUE is the region where to deploy the resources
+- AWS_ACCESS_KEY_ID_VALUE and AWS_SECRET_ACCESS_KEY_VALUE are credentials of the IAM AWS user used to login on AWS Cli
+
+
+### Third step summary
+- Checks whether the env variables are initialized
+- Confirms the stacks (s3, rds, lambda) exist
+- Deploys the S3 stack and apply the policies
+- Retrieves RDS endpoint and port and S3 bucket name then deploys the lambda stack and apply the triggers to S3. The names of s3 and rds stacks are retrieved by the AWS_STACK_PREFIX and by appending “s3” or “rds”.
+- Deletes the packages as a clean up
---
+++ entrypoint.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+
+echo "JOB STARTED"
+
+set -e
+
+echo "Check env variables STARTED"
+
+if [[ ! -z "$AWS_FOLDER" ]]; then
+    cd $AWS_FOLDER
+fi
+
+if [[ -z "$TEMPLATE_S3" ]]; then
+    echo "Empty template specified. Looking for s3Stack.yml..."
+
+    if [[ ! -f "s3Stack.yml" ]]; then
+        echo s3Stack.yml not found
+        exit 1
+    fi
+
+    TEMPLATE_S3="s3Stack.yml"
+fi
+
+if [[ -z "$TEMPLATE_RDS" ]]; then
+    echo "Empty template specified. Looking for rdsStack.yml..."
+
+    if [[ ! -f "rdsStack.yml" ]]; then
+        echo rdsStack.yml not found
+        exit 1
+    fi
+
+    TEMPLATE_RDS="rdsStack.yml"
+fi
+
+if [[ -z "$TEMPLATE_LAMBDA" ]]; then
+    echo "Empty template specified. Looking for lambdaStack.yml..."
+
+    if [[ ! -f "lambdaStack.yml" ]]; then
+        echo lambdaStack.yml not found
+        exit 1
+    fi
+
+    TEMPLATE_LAMBDA="lambdaStack.yml"
+fi
+
+if [[ -z "$AWS_ACCESS_KEY_ID_VALUE" ]]; then
+    echo AWS Access Key ID invalid
+    exit 1
+fi
+
+if [[ -z "$AWS_SECRET_ACCESS_KEY_VALUE" ]]; then
+    echo AWS Secret Access Key invalid
+    exit 1
+fi
+
+if [[ -z "$AWS_REGION_VALUE" ]]; then
+    echo AWS Region invalid
+    exit 1
+fi
+
+if [[ ! -z "$AWS_DEPLOY_BUCKET" ]]; then
+    unset $AWS_DEPLOY_BUCKET
+fi
+
+if [[ -z "$AWS_STACK_PREFIX" ]]; then
+    echo "You haven't declared any AWS_STACK_PREFIX. Using the branch name instead."
+    AWS_STACK_PREFIX="$(echo ${GITHUB_REF#refs/heads/})"
+fi
+
+if [[ ! -z "$AWS_BUCKET_PREFIX" ]]; then
+    AWS_BUCKET_PREFIX="--s3-prefix ${AWS_BUCKET_PREFIX}"
+fi
+
+if [[ $FORCE_UPLOAD == true ]]; then
+    FORCE_UPLOAD="--force-upload"
+fi
+
+if [[ $NO_FAIL_EMPTY_CHANGESET == true ]]; then
+    NO_FAIL_EMPTY_CHANGESET="--no-fail-on-empty-changeset"
+fi
+
+if [[ $USE_JSON == true ]]; then
+    USE_JSON="--use-json"
+fi
+
+if [[ -z "$CAPABILITIES" ]]; then
+    CAPABILITIES="--capabilities CAPABILITY_IAM"
+else
+    CAPABILITIES="--capabilities $CAPABILITIES"
+fi
+
+if [[ ! -z "$PARAMETER_OVERRIDES" ]]; then
+    PARAMETER_OVERRIDES="--parameter-overrides $PARAMETER_OVERRIDES"
+fi
+
+if [[ ! -z "$TAGS" ]]; then
+    TAGS="--tags $TAGS"
+fi
+
+mkdir -p ~/.aws
+touch ~/.aws/credentials
+touch ~/.aws/config
+
+echo "[default]
+aws_access_key_id = $AWS_ACCESS_KEY_ID_VALUE
+aws_secret_access_key = $AWS_SECRET_ACCESS_KEY_VALUE
+region = $AWS_REGION_VALUE" > ~/.aws/credentials
+
+echo "[default]
+output = text
+region = $AWS_REGION_VALUE" > ~/.aws/config
+
+
+STACKS3="${AWS_STACK_PREFIX}-${AWS_REGION_VALUE}-s3"
+STACKRDS="${AWS_STACK_PREFIX}-${AWS_REGION_VALUE}-rds"
+STACKLAMBDA="${AWS_STACK_PREFIX}-${AWS_REGION_VALUE}-lambda"
+AWS_DEPLOY_BUCKET="${AWS_STACK_PREFIX}-${AWS_REGION_VALUE}-packages-bucket"
+
+if [[ -z "$(aws s3api list-buckets --query "Buckets[?Name=='${AWS_DEPLOY_BUCKET}'].Name" --output text)" ]]; then 
+    echo "Creating a new s3 to store packages!"
+    aws s3 mb s3://$AWS_DEPLOY_BUCKET --region $AWS_REGION_VALUE
+fi
+
+echo "Check variables COMPLETED"
+
+#CHECK STACKS DON'T ALREADY EXIST
+
+echo "Check if stack exists STARTED"
+
+CHECK_STACK_S3=$(aws cloudformation describe-stacks \
+--region ${AWS_REGION_VALUE} \
+--query "Stacks[?StackName=='${STACKS3}'].StackName")
+
+if [[ -z "$CHECK_STACK_S3" ]]; then
+    echo "ERROR! The S3 Stack doesn't exist! Deploy it before updating the lambda functions!"
+    exit 1
+fi
+
+CHECK_STACK_RDS=$(aws cloudformation describe-stacks \
+--region ${AWS_REGION_VALUE} \
+--query "Stacks[?StackName=='${STACKRDS}'].StackName")
+
+if [[ -z "$CHECK_STACK_RDS" ]]; then
+    echo "ERROR! The RDS Stack doesn't exist! Deploy it before updating the lambda functions!"
+    exit 1
+fi
+
+CHECK_STACK_LAMBDA=$(aws cloudformation describe-stacks \
+--region ${AWS_REGION_VALUE} \
+--query "Stacks[?StackName=='${STACKLAMBDA}'].StackName")
+
+if [[ -z "$CHECK_STACK_LAMBDA" ]]; then
+    echo "ERROR! The Lambda Stack doesn't exist! Deploy it before updating the lambda functions!"
+    exit 1
+fi
+
+echo "Check if stack exists COMPLETED"
+
+#CREATE PACKAGES
+
+echo "Creation stack package STARTED"
+
+
+## EVENTUALLY INSTALLING DEPENDENCIES
+cd lambda_functions/dependencies/nodejs && npm install && cd ../../..
+
+aws cloudformation package --template-file $TEMPLATE_LAMBDA --output-template-file lambdapackage.yml \
+--s3-bucket $AWS_DEPLOY_BUCKET $AWS_BUCKET_PREFIX $FORCE_UPLOAD $USE_JSON
+
+
+echo "Creation stack package COMPLETED"
+
+#RETRIEVE STACKS' INFOS FOR LAMBDA STACK
+
+echo "Retrieving of s3 and rds stacks' outputs STARTED"
+
+S3NAME=$(aws cloudformation describe-stacks \
+  --stack-name $STACKS3 \
+  --query "Stacks[0].Outputs[?OutputKey=='S3BucketInstanceName'].OutputValue" \
+  --output text
+)
+
+S3ARN=$(aws cloudformation describe-stacks \
+  --stack-name $STACKS3 \
+  --query "Stacks[0].Outputs[?OutputKey=='S3BucketInstanceArn'].OutputValue" \
+  --output text
+)
+
+RDSADDRESS=$(aws cloudformation describe-stacks \
+  --stack-name $STACKRDS \
+  --query "Stacks[0].Outputs[?OutputKey=='RDSInstanceAddress'].OutputValue" \
+  --output text
+)
+
+RDSPORT=$(aws cloudformation describe-stacks \
+  --stack-name $STACKRDS \
+  --query "Stacks[0].Outputs[?OutputKey=='RDSInstancePort'].OutputValue" \
+  --output text
+)
+
+echo "Retrieving of s3 and rds stacks' outputs COMPLETED"
+
+#DEPLOY LAMBDA STACK
+
+echo "Deploy lambda stack STARTED"
+
+aws cloudformation deploy --template-file lambdapackage.yml \
+--capabilities CAPABILITY_NAMED_IAM --stack-name $STACKLAMBDA \
+--parameter-overrides S3StackName=$STACKS3 S3BucketName=$S3NAME S3BucketArn=$S3ARN \
+RDSStackName=$STACKRDS RDSInstanceAddress=$RDSADDRESS RDSInstancePort=$RDSPORT \
+BranchName=$AWS_STACK_PREFIX \
+$TAGS $NO_FAIL_EMPTY_CHANGESET
+
+echo "Deploy lambda stack COMPLETED"
+
+#CREATE TRIGGER FOR S3 AND LAMBDA
+
+echo "Creation s3 triggers STARTED"
+
+TRANSCODEARN=$(aws cloudformation describe-stacks \
+  --stack-name $STACKLAMBDA \
+  --query "Stacks[0].Outputs[?OutputKey=='transcodeVideo'].OutputValue" \
+  --output text
+)
+
+ONVIDEOARN=$(aws cloudformation describe-stacks \
+  --stack-name $STACKLAMBDA \
+  --query "Stacks[0].Outputs[?OutputKey=='onVideoTranscoded'].OutputValue" \
+  --output text
+)
+
+JSON=$(cat <<-EOF
+{
+    "LambdaFunctionConfigurations": [
+        {
+            "Id": "transcodeVideo",
+            "LambdaFunctionArn": "$TRANSCODEARN",
+            "Events": [
+                "s3:ObjectCreated:*"
+            ],
+            "Filter": {
+                "Key": {
+                    "FilterRules": [
+                        {
+                            "Name": "prefix",
+                            "Value": "inputs/"
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "Id": "onVideoTranscoded",
+            "LambdaFunctionArn": "$ONVIDEOARN",
+            "Events": [
+                "s3:ObjectCreated:*"
+            ],
+            "Filter": {
+                "Key": {
+                    "FilterRules": [
+                        {
+                            "Name": "prefix",
+                            "Value": "outputs/"
+                        }
+                    ]
+                }
+            }
+        }
+        ]
+}
+EOF
+)
+
+aws s3api \
+  put-bucket-notification-configuration \
+  --bucket $S3NAME \
+  --notification-configuration "$JSON"
+
+echo "Creation s3 triggers COMPLETED"
+
+#CLEAN PACKAGES
+echo "Clean packages STARTED"
+rm lambdapackage.yml
+echo "Clean packages COMPLETED"
+
+echo "Job COMPLETED"
